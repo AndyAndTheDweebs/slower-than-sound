804a6c172205359b8289bab9346ecd9fb5861146
tyguy56 <tyguy56@gmail.com>
2021-11-08 01:36:48 -0600

    reformatted code

--- src/ship.rs
+++ src/ship.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::*;
 use crate::constants::*;
+use bevy::prelude::*;
 
 struct ShipMaterialResource {
     ship_texture: Handle<ColorMaterial>,
@@ -14,7 +14,7 @@ pub struct _texture;
 * type: initalization
 *
 * description: used to allow bevy to handle resource initalization once .init_resource is called
-*   
+*
 * return: none
 */
 
@@ -27,8 +27,8 @@ impl FromWorld for ShipMaterialResource {
 
         let ship_handle = asset_server.load("ship.png");
 
-        ShipMaterialResource{
-            ship_texture: materials.add(ship_handle.into())
+        ShipMaterialResource {
+            ship_texture: materials.add(ship_handle.into()),
         }
     }
 }
@@ -43,19 +43,23 @@ impl FromWorld for ShipMaterialResource {
 * parameters:
 *   commands: mutable variable used to execute commands
 *   materials: needed for loading sprites
-*   
+*
 * return: none
 */
 
-fn spawn_ship(
-    mut commands: Commands,
-    materials: Res<ShipMaterialResource>,
-) {
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.ship_texture.clone(),
-        sprite: Sprite{ size: Vec2::new(550.0, 250.0), flip_x: true, flip_y: false, resize_mode: SpriteResizeMode::Manual },
-        ..Default::default()
-    }).insert(_texture);
+fn spawn_ship(mut commands: Commands, materials: Res<ShipMaterialResource>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.ship_texture.clone(),
+            sprite: Sprite {
+                size: Vec2::new(550.0, 250.0),
+                flip_x: true,
+                flip_y: false,
+                resize_mode: SpriteResizeMode::Manual,
+            },
+            ..Default::default()
+        })
+        .insert(_texture);
 }
 
 /*
@@ -63,8 +67,8 @@ fn spawn_ship(
 *
 * type: plugin
 *
-* description: creation of plugin for ship, used to be called 
-*   
+* description: creation of plugin for ship, used to be called
+*
 * return: none
 */
 
@@ -72,8 +76,9 @@ pub struct ShipPlugin;
 impl Plugin for ShipPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.init_resource::<ShipMaterialResource>()
-        //.add_state(AppState::InGame)
-        .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(spawn_ship.system()));
+            //.add_state(AppState::InGame)
+            .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(spawn_ship.system()));
+        //.add_system_set(SystemSet::on_update(AppState::InGame).with_system(test_state.system()));
         //.add_startup_system(spawn_ship.system());
     }
 }
